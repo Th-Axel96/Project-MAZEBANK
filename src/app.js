fa3d3bb396e02185c8040945523a45041840fd62
Th-Axel96 <axelmoussart77@hotmail.com>
2022-08-24 12:56:36 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import "./assets/css/styles.css";
+// import _ from "lodash";
+// import Icon from './assets/img/img-test-webpack.jpg';
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(['', ''], ' ');
+//   element.classList.add("text-3xl");
+
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
+
+//   element.appendChild(myIcon);
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+
+console.log("Hello JS");
